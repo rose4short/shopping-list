d3eb5f0079e358e461adb0d044b9fe13317e3fe2
Rose Johnson <johnso3x@gmail.com>
2019-06-18 14:28:59 -0400

    delete items

--- index.js
+++ index.js
@@ -22,6 +22,27 @@ function addItemsToList(){
         const listItem= htmlForItem(itemString)
         //put list item at the bottom of list
         $('ul.shopping-list').append(listItem);
+
+        
     })
 }
+function checkItems(){ 
+
+    $( ".shopping-item-toggle" ).on('click', function(event){
+        // find the span 
+        console.log('check button clicked')
+        let span=$(event.currentTarget).closest("li").find("span.shopping-item");
+        //toggle class to the span element
+        span.toggleClass( "shopping-item__checked" );
+    })
+}
+function deleteItems(){
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+         //find closet li and remove it
+        $(this).closest('li').remove();
+    })
+}
+
 $(addItemsToList);
+$(checkItems);
+$(deleteItems);
